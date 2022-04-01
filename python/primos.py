cfe4a4f84052400aad70bf186a80eb710924d279
Gonzze <errandoneagonzalo18@gmail.com>
2022-04-01 15:19:42 -0300

    punto 7

--- python/primos.py
+++ python/primos.py
@@ -1,34 +1,36 @@
-# prime number calculator: find all primes up to n
-max = int(input("Find primes up to what number? : "))
-primeList = []
-#for loop for checking each number
-for x in range(2, max + 1):
-	isPrime = True
-	index = 0
-	root = int(x ** 0.5) + 1
-	while index < len(primeList) and primeList[index] <= root:
-		if x % primeList[index] == 0:
-			isPrime = False
-			break
-		index += 1
-	if isPrime:
-		primeList.append(x)
-print(primeList)
-#-------------------------------------------------------------
-# prime number calculator: find the first n primes
-count = int(input("Find how many primes?: "))
-primeList = []
-x = 2
-while len(primeList) < count:
-	isPrime = True
-	index = 0
-	root = int(x ** 0.5) + 1
-	while index < len(primeList) and primeList[index] <= root:
-		if x % primeList[index] == 0:
-			isPrime = False
-			break
-		index += 1
-	if isPrime:
-		primeList.append(x)
-	x += 1
-print(primeList)
+def imprimirNumerosPrimos(maximo = 100):
+	primeList = []
+	#Imprime los numeros primos hasta el indicado por el usuario
+	for x in range(2, maximo + 1):
+		isPrime = True
+		index = 0
+		root = int(x ** 0.5) + 1
+		while index < len(primeList) and primeList[index] <= root:
+			if x % primeList[index] == 0:
+				isPrime = False
+				break
+			index += 1
+		if isPrime:
+			primeList.append(x)
+	print(primeList)
+
+def imprimirNCantidadDeNumerosPrimos(cantidad = 10):
+	#-------------------------------------------------------------
+	# Imprime n cantidad de numeros primos, cuantos quiera el usuario
+
+	primeList = []
+	x = 2
+	
+	while len(primeList) < cantidad:
+		isPrime = True
+		index = 0
+		root = int(x ** 0.5) + 1
+		while index < len(primeList) and primeList[index] <= root:
+			if x % primeList[index] == 0:
+				isPrime = False
+				break
+			index += 1
+		if isPrime:
+			primeList.append(x)
+		x += 1
+	print(primeList)
